b3826c14fa0c911a37245f69c6dbddfdf7966cee
MarcoEYC <m.yovera@pucp.edu.pe>
2024-06-10 12:38:50 -0500

    feat: AFVAL Modification In main application class and maven dependencies and config

---
+++ antifraud-validation-service/src/test/java/com/yape/codechallenge/antifraudvalidationservice/AntifraudValidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yape.codechallenge.antifraudvalidationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntifraudValidationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
